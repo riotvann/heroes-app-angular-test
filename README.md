bd167f59a3116be47e8a7b180e21a98d56d15a2d
jgamesl <jgamezldev@gmail.com>
2024-08-19 09:33:55 -0700

    add basic auth

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Dev
 
 1. Clonar el poroyecto
-2. Ejecutar npm install 
-3. Levantar backend npm run backend
-4. Ejecutar la app npm start o bien ng serve -o
+2. Ejecutar ```npm install``` 
+3. Levantar backend ```npm run backend```
+4. Ejecutar la app ```npm start``` o bien ```ng serve -o```
---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,66 @@
+// import { Injectable } from '@angular/core';
+// import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+
+// @Injectable({providedIn: 'root'})
+// export class AuthGuard implements CanMatch, CanActivate{
+//     constructor() { }
+
+//     canMatch(route: Route, segments: UrlSegment[]): MaybeAsync<GuardResult> {
+//         throw new Error('Method not implemented.');
+//     }
+//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+//         throw new Error('Method not implemented.');
+//     }
+
+
+// }
+
+import {
+    ActivatedRouteSnapshot,
+    CanActivateFn,
+    CanMatchFn,
+    Route,
+    Router,
+    RouterStateSnapshot,
+    UrlSegment,
+} from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+
+//No hay necesidad de crear una clase, simplemente definiendo una función flecha y exportándola podemos utilizar sus funcionalidades de guard en el app-routing
+export const canActivateGuardPublic: CanActivateFn = ( //Hay que tener en cuenta el tipado CanActiveFn
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+) => {
+    console.log('CanActivate');
+    console.log({ route, state });
+
+    return checkAuthStatus();
+};
+
+export const canMatchGuardPublic: CanMatchFn = ( //Tipado CanMatchFN
+    route: Route,
+    segments: UrlSegment[]
+) => {
+    console.log('CanMatch');
+    console.log({ route, segments });
+
+    return checkAuthStatus();
+};
+
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+    //se inyectan el AuthService y el Router
+    const authService: AuthService = inject(AuthService);
+    const router: Router = inject(Router);
+
+    return authService.checkAuthentication().pipe(
+        tap((isAuthenticated) => {
+            if (isAuthenticated) {
+                router.navigate(['/']);
+            }
+        }),
+        map(isAuthenticated => !isAuthenticated)
+    );
+};
